8f61195f016505dab313c7ef081516e6db597200
Bulat Maskurov <b.maskurov@uzum.com>
2023-01-18 11:27:29 +0500

    resolving merge conflicts

--- snow-boarder/Assets/Scripts/CrashDetector.cs
+++ snow-boarder/Assets/Scripts/CrashDetector.cs
@@ -2,25 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class CrashDetector : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-=======
 public class CrashDetector : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground") {
             Debug.Log("Ouch, hit my head");
         }
->>>>>>> 8ec949de355f531f28ab6eea51fdbc4b63af7803
     }
 }
--- snow-boarder/Assets/Scripts/FinishLine.cs
+++ snow-boarder/Assets/Scripts/FinishLine.cs
@@ -2,25 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class FinishLine : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-=======
 public class FinishLine : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             Debug.Log("You finished!");
         }
->>>>>>> 8ec949de355f531f28ab6eea51fdbc4b63af7803
     }
 }
