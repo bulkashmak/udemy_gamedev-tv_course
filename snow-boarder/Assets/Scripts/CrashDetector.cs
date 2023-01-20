9c7a85e7a139037f7b3f867caed82f65916cb61f
Bulat Maskurov <b.maskurov@uzum.com>
2023-01-20 18:24:19 +0500

    Triggering Particle Effects

--- snow-boarder/Assets/Scripts/CrashDetector.cs
+++ snow-boarder/Assets/Scripts/CrashDetector.cs
@@ -6,10 +6,12 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour {
 
     [SerializeField] float loadSceneDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     void OnTriggerEnter2D(Collider2D other) {
 
         if (other.tag == "Ground") {
+            crashEffect.Play();
             Invoke("ReloadScene", loadSceneDelay);
         }
     }
